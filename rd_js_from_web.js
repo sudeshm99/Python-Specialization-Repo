648c0bcbfa91147c6f1c9aaa94a8f5f1e32a2b36
Sudesh Madushanka <sudesh.madushanka25@gmail.com>
2019-02-27 07:39:00 +0530

    read data from json assignment

--- rd_js_from_web.js
+++ rd_js_from_web.js
@@ -1,7 +1,29 @@
 import urllib.request, urllib.parse, urllib.error
-import jason
+import json
 
+sumofcunts=0
+count=0
 while True:
 	inpt=input("Enter url here: ")
 	if len(inpt)<1:
 		break
+	data=''
+	try:
+		fh=urllib.request.urlopen(inpt)
+		data=fh.read().decode()
+	except:
+		print("Unknown url type",inpt)
+		break
+	print("Retriving from:",inpt)
+	print("Retrived",len(data),"charactors")
+	js=json.loads(data)
+	comments=js["comments"]
+	for comment in comments:
+		val=int(comment["count"])
+		sumofcunts+=val
+		count+=1
+	print("counts:",count)
+	print("sum:",sumofcunts)
+
+
+
