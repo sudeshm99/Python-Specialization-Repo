267e9a38d69fd5e1008cb4bd96189684266c76ba
Sudesh Madushanka <sudesh.madushanka25@gmail.com>
2019-03-26 16:11:17 +0530

    complete search engine

---
+++ spider.js
@@ -0,0 +1,14 @@
+spiderJson = {"nodes":[
+{"weight":1,"rank":2.71428571428559, "id":4, "url":"http://python-data.dr-chuck.net/comments_42.html"},
+{"weight":1,"rank":19.0, "id":7, "url":"http://python-data.dr-chuck.net/known_by_42.html"},
+{"weight":1,"rank":19.0, "id":12, "url":"http://python-data.dr-chuck.net/known_by_Jasmyn.html"},
+{"weight":1,"rank":19.0, "id":102, "url":"http://python-data.dr-chuck.net/known_by_Kerriannah.html"},
+{"weight":1,"rank":0.0, "id":212, "url":"http://python-data.dr-chuck.net/known_by_Yasmeen.html"},
+{"weight":1,"rank":0.0, "id":228, "url":"http://python-data.dr-chuck.net/known_by_Amylea.html"},
+{"weight":1,"rank":0.0, "id":281, "url":"http://python-data.dr-chuck.net/known_by_Lucien.html"}],
+"links":[
+{"source":1,"target":2,"value":3},
+{"source":1,"target":3,"value":3},
+{"source":3,"target":4,"value":3},
+{"source":3,"target":5,"value":3},
+{"source":3,"target":6,"value":3}]};
